7d8290b53d9834fa212f9d98b8cc0090f51480a4
Sandeep Raghunandhan <saraghun@ucsd.edu>
2017-01-04 23:38:27 -0800

    Initial Commit

---
+++ Headline/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Headline
+//
+//  Created by Sandeep Raghunandhan on 1/4/17.
+//  Copyright © 2017 Sandeep Raghunandhan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
